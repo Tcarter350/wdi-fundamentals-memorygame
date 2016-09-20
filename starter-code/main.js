c8135f4a81a67e514a477aa17a37eba21266ac21
Tcarter350 <theacarter@ymail.com>
2016-09-20 18:52:07 +0100

    javascript active

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,4 +4,39 @@ var cardTwo="queen";
 var cardThree="king";
 var cardFour="king";
 
+if (cardTwo === CardFour) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
 
+if (cardTwo === cardThree) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
+
+if (cardTwo === cardOne) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
+
+if (cardOne === cardThree) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
+
+if (cardOne === cardFour) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
+
+
+if (cardThree === cardFour) {
+	alert('you found a match!');
+} else {
+	alert('sorry, try again!');
+}
